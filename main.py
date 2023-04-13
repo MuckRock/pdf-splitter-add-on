ad99cad106cf7fdae381afabd4fa62eb0e234c10
s <102841251+duckduckgrayduck@users.noreply.github.com>
2023-04-13 14:50:33 -0500

    Ran black and pylint

--- main.py
+++ main.py
@@ -2,28 +2,32 @@
 This Add-On uses pdftk: https://www.pdflabs.com/tools/pdftk-the-pdf-toolkit/ to 
 split a DocumentCloud document into two documents and uploads the two resulting documents. 
 """
-
-from documentcloud.addon import AddOn
 import os
 import subprocess
+from documentcloud.addon import AddOn
+
 
 class Split(AddOn):
     """Creates a temp directory, downloads doc, splits along page, uploads split documents"""
 
     def main(self):
+        """Runs the document through pdfkit and splits across that page,
+        then uploads two output documents"""
         page = self.data["page"]
         page2 = page + 1
         os.makedirs(os.path.dirname("./out/"), exist_ok=True)
         for document in self.get_documents():
             title = document.title
-            with open(f'{title}.pdf', 'wb') as f:
-                f.write(document.pdf)
-            cmd1 = f'pdftk "{title}.pdf" cat 1-{page} output "./out/{title}_1-{page}.pdf"'
+            with open(f"{title}.pdf", "wb") as file:
+                file.write(document.pdf)
+            cmd1 = (
+                f'pdftk "{title}.pdf" cat 1-{page} output "./out/{title}_1-{page}.pdf"'
+            )
             cmd2 = f'pdftk "{title}.pdf" cat {page2}-end output "./out/{title}_{page2}-end.pdf"'
             subprocess.call(cmd1, shell=True)
             subprocess.call(cmd2, shell=True)
         self.client.documents.upload_directory("./out/")
-            
+
 
 if __name__ == "__main__":
     Split().main()
